76545211c10a6d57a6b0293818b8ea8c3f156d26
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-08 18:17:09 +0800

    initial commit

---
+++ variables.py
@@ -0,0 +1,13 @@
+# Given the variables defined here, write Python 
+# code that prints out the distance, in meters, 
+# that light travels in one processor cycle. 
+
+speed_of_light = 299792458.0 # meters per second
+cycles_per_second = 2700000000.0 #2.7 Ghz
+
+cycle_distance = speed_of_light/cycles_per_second
+
+print cycle_distance
+
+
+
